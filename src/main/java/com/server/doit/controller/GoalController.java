7aa2b28098f072b75432da1f95d5069c9f7de517
MaKyuSeok <mks502@naver.com>
2019-07-14 21:54:59 +0900

    invite to goal and participate to goal API FIN
    골에대해 초대를 하는 코드 생성 api와
    초대에 응해 해당 골에 참여하는 API

--- src/main/java/com/server/doit/controller/GoalController.java
+++ src/main/java/com/server/doit/controller/GoalController.java
@@ -2,13 +2,18 @@ package com.server.doit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.server.doit.controller.dto.GoalDto;
 import com.server.doit.domain.entity.Goal;
+import com.server.doit.domain.entity.Participant;
+import com.server.doit.domain.repository.InviteInfoRepository;
 import com.server.doit.domain.service.GoalService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,4 +38,27 @@ public class GoalController {
 
         return ResponseEntity.ok().body(goal);
     }
+    
+    @GetMapping("/api/goal/invite/from/{mid}/goal/{gid}")
+    public ResponseEntity<Long> inviteGoal(@PathVariable String mid,@PathVariable String gid){
+    	Long goalId = Long.parseLong(gid);
+		Long memId = Long.parseLong(mid);
+    	Long inviteCode = goalService.invite(memId, goalId);
+    	if(inviteCode == null) return ResponseEntity.badRequest().body(null);
+    	
+    	return ResponseEntity.ok().body(inviteCode);
+    }
+    
+    @PutMapping("/api/goal/participate/code/{inviteId}/mid/{mid}/gid/{gid}")
+    public ResponseEntity<Participant> participateGoal(@PathVariable String inviteId,@PathVariable String mid,@PathVariable String gid){
+    	Long goalId = Long.parseLong(gid);
+		Long memId = Long.parseLong(mid);
+		Long code = Long.parseLong(inviteId);
+		
+		Participant participant = goalService.participateGoal(code,goalId, memId);	
+		
+		if (participant == null) return ResponseEntity.badRequest().body(null);
+        return ResponseEntity.ok().body(participant);
+    }
+    
 }
