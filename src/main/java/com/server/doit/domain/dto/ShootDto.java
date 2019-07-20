51177eab11e948299a479e07a6afe699807083d7
cjh5414 <cjh5414@linecorp.com>
2019-07-20 14:21:42 +0900

    Add base time

--- src/main/java/com/server/doit/domain/dto/ShootDto.java
+++ src/main/java/com/server/doit/domain/dto/ShootDto.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class ShootDto {
     private Long gid;
     private Long time;
+    private Long baseTime;
     private String text;
     private Long sid;
 	private Long mid;        //슛을 만든 사람 mid
