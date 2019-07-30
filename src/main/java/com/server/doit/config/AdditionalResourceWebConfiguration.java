e5b737279ed4171c3fde18edebdbc4eadfe81d5a
cjh5414 <cjh5414@linecorp.com>
2019-07-30 14:51:03 +0900

    Add resource handler

---
+++ src/main/java/com/server/doit/config/AdditionalResourceWebConfiguration.java
@@ -0,0 +1,13 @@
+package com.server.doit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("file:static/");
+    }
+}
